6dc5e22aa6ca40fd5c6a77a3d4074b7bb0302816
Danny Eck <danny.eck@osi.ca.gov>
2018-03-06 15:39:14 -0800

    WiP

--- app/javascript/Dashboard.jsx
+++ app/javascript/Dashboard.jsx
@@ -1,59 +1,70 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 import { GlobalHeader, PageHeader } from 'react-wood-duck';
 
-const Dashboard = ({name}) => (
-    <div>
-        <GlobalHeader />
-        <PageHeader
-            pageTitle="Services & Resources"
-            button={null}
-        />
-        <div className="container">
+const SERVICES = [
+  {
+    name: 'Snapshot',
+    href: '',
+  },
+  {
+    name: 'Hotline',
+    href: '',
+  },
+  {
+    name: 'CALS (Facilities)',
+    href: '',
+  },
+  {
+    name: 'CALS (RFA)',
+    href: '',
+  },
+  {
+    name: 'Case Management',
+    href: '',
+  },
+];
 
-            <h2>Services</h2>
+const RESOURCES = [
+  {
+    name: 'CWS-CARES Training On-Demand',
+  },
+  {
+    name: 'Release Notes',
+  },
+];
 
-            <div className="row">
-                <div className="col-md-4 col-sm-2">
-                asdfasdf
-                </div>
-                <div className="col-md-4 col-sm-2">
-                asdfasdf
-                </div>
-                <div className="col-md-4 col-sm-2">
-                asdfasdf
-                </div>
-                <div className="col-md-4 col-sm-2">
-                asdfasdf
-                </div>
-            </div>
-
-            <h2>Resources</h2>
-
-        <div className="row">
-                <div className="col-md-4 col-sm-2">
-                asdfasdf
-                </div>
-                <div className="col-md-4 col-sm-2">
-                asdfasdf
-                </div>
-                <div className="col-md-4 col-sm-2">
-                asdfasdf
-                </div>
-                <div className="col-md-4 col-sm-2">
-                asdfasdf
-                </div>
-            </div>
-        </div>
+const Dashboard = ({ name }) => (
+  <div>
+    <GlobalHeader />
+    <PageHeader pageTitle="Services & Resources" button={null} />
+    <div className="container">
+      <h2>Services</h2>
+      <div className="row">
+        {SERVICES.map(({ name }) => (
+          <div className="col-md-4 col-sm-6" key={name}>
+            {name}
+          </div>
+        ))}
       </div>
-)
+      <h2>Resources</h2>
+      <div className="row">
+        {RESOURCES.map(({ name }) => (
+          <div className="col-md-4 col-sm-6" key={name}>
+            {name}
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+);
 
 Dashboard.defaultProps = {
-  name: 'David'
-}
+  name: 'David',
+};
 
 Dashboard.propTypes = {
-  name: PropTypes.string
-}
+  name: PropTypes.string,
+};
 
 export default Dashboard;
---
+++ app/javascript/components/Tile/Tile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Tile = ({}) => {
+  return <div>alskdfj</div>;
+};
+
+Tile.propTypes = {};
+Tile.defaultProps = {};
+
+export default Tile;

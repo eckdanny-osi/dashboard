27374b4278fc6b56a7f1c454f5504faa4c810f04
Danny Eck <danny.eck@osi.ca.gov>
2018-03-08 00:01:24 -0800

    Clean up more wack

--- app/controllers/sandbox_base_controller.rb
+++ app/controllers/sandbox_base_controller.rb
@@ -5,7 +5,7 @@ class SandboxBaseController < ApplicationController
 
   def authenticate_with_cwds
 
-    token = Cwds::Authentication.token_generation(params[:accessCode], ENV.fetch('PERRY_URL'))
+    token = Cwds::Authentication.token_generation(params[:accessCode], ENV.fetch('PERRY_BASE_URL'))
 
     puts "beofre - session token #{session[:token]}"
 
@@ -20,18 +20,18 @@ class SandboxBaseController < ApplicationController
     # If no new token and current token exists, continue as normal
     return if session[:token]
 
-    if Cwds::Authentication.token_validation(new_token, ENV.fetch('PERRY_URL'))
+    if Cwds::Authentication.token_validation(new_token, ENV.fetch('PERRY_BASE_URL'))
       puts "token validated success #{new_token}"
       session[:token] = new_token
 
-      profile = Cwds::Authentication.store_user_details_from_token(new_token, ENV.fetch('PERRY_URL'))
+      profile = Cwds::Authentication.store_user_details_from_token(new_token, ENV.fetch('PERRY_BASE_URL'))
       session[:profile] = profile
 
       puts "will redirect to - #{url_for(request.params.except(:token))}"
       # redirect to request URL without token in querystring
       redirect_to url_for(request.params.except(:token))
     else
-      redirect_to Cwds::Authentication.authentication_url(ENV.fetch('LOGIN_URL'), request.original_url)
+      redirect_to Cwds::Authentication.authentication_url(ENV.fetch('LOGIN_BASE_URL'), request.original_url)
     end
   end
 
